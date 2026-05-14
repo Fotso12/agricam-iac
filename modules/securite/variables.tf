28c789998e188ee96d9bd13518c1cb1e389538a3
Fotso12 <tamofotso90@gmail.com>
2026-05-14 15:47:12 +0100

    Correction

--- modules/securite/variables.tf
+++ modules/securite/variables.tf
@@ -5,7 +5,4 @@ variable "suffix" {
   description = "Suffixe unique pour les noms de buckets S3"
   type        = string
 }
-variable "aws_region" {
-  type        = string
-  description = "Region AWS pour les services logs"
-}
+
